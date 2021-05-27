3a8105bcd5fe23d8a5e078d4a97f71bc30679407
Timothy Kristanto <66807730+TimothyKristanto@users.noreply.github.com>
2021-05-27 14:18:36 +0700

    Created Menu, order list, and order history

---
+++ OrDine/app/src/main/java/com/example/ordine/OrderListActivity.java
@@ -0,0 +1,14 @@
+package com.example.ordine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+    }
+}
--- OrDine/app/src/main/java/com/example/ordine/RegisterPage.java
+++ OrDine/app/src/main/java/com/example/ordine/RegisterPage.java
@@ -1,14 +1,29 @@
 package com.example.ordine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterPage extends AppCompatActivity {
+    private TextView txtLoginRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        txtLoginRegister = findViewById(R.id.txtLoginRegister);
+
+        txtLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
