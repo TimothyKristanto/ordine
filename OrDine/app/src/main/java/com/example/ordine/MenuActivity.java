c18143e411e4d727576cc053d3b18018ff75c0e2
jverdianto <84266180+jeffreyverdianto@users.noreply.github.com>
2021-05-28 15:40:37 +0700

    Added food rating
    Added Menu navigation

--- OrDine/app/src/main/java/com/example/ordine/MenuActivity.java
+++ OrDine/app/src/main/java/com/example/ordine/MenuActivity.java
@@ -1,14 +1,46 @@
 package com.example.ordine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private ImageView imgPrevMenu;
+    private ImageView imgNextMenu;
+    private RecyclerView recyclerviewMenu;
+    private Button btnAddMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        imgPrevMenu = findViewById(R.id.imgPrevMenu);
+        imgNextMenu = findViewById(R.id.imgNextMenu);
+        recyclerviewMenu = findViewById(R.id.recyclerviewMenu);
+        btnAddMenu = findViewById(R.id.btnAddMenu);
+
+        imgPrevMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), OrderHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgPrevMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), OrderListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
